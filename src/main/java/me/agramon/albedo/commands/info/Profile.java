a09e5dc88ee9ccb41a30abe085e11eae22d6aa34
xAgramon <30298077+xAgramon@users.noreply.github.com>
2019-12-14 23:22:33 -0500

    Albedo v1.2.2
    -> Boobs is a img/gif command now
    -> Added ability to get profile of a user by userID
    -> Updated JDA to v4.0.0_74

--- src/main/java/me/agramon/albedo/commands/info/Profile.java
+++ src/main/java/me/agramon/albedo/commands/info/Profile.java
@@ -9,7 +9,7 @@ import com.mongodb.client.MongoDatabase;
 import me.agramon.albedo.Config;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Emote;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import org.bson.Document;
 
 import java.awt.*;
@@ -26,12 +26,14 @@ public class Profile extends Command {
 
     @Override
     protected void execute(CommandEvent e) {
-        Member name;
+        User user;
 
         if (e.getArgs().isEmpty()) {
-            name = e.getMember();
+            user = e.getMember().getUser();
         } else if (e.getMessage().getMentionedMembers().size() != 0){
-            name = e.getMessage().getMentionedMembers().get(0);
+            user = e.getMessage().getMentionedMembers().get(0).getUser();
+        } else if (e.getJDA().getUserById(e.getArgs()) != null) {
+            user = e.getJDA().getUserById(e.getArgs());
         } else {
             e.reply("Invalid arguments! The command is >profile <@user>");
             return;
@@ -44,7 +46,7 @@ public class Profile extends Command {
         MongoDatabase db = mongoClient.getDatabase("Albedo");
         MongoCollection<Document> collection = db.getCollection("Anime Argonauts");
 
-        Document found = collection.find(new Document("UserID", name.getId())).first();
+        Document found = collection.find(new Document("UserID", user.getId())).first();
         if (found != null) {
             adores = found.get("Adores") + "";
             credits = found.get("Credits") + "";
@@ -57,11 +59,11 @@ public class Profile extends Command {
 
         EmbedBuilder eb = new EmbedBuilder()
                 .setColor(Color.MAGENTA)
-                .setThumbnail(name.getUser().getAvatarUrl())
-                .setAuthor("Profile of " + name.getUser().getName(), null, name.getUser().getAvatarUrl())
-                .addField("Username:", name.getUser().getName(), false)
-                .addField("Account Created:", name.getUser().getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
-                .addField("Server Joined:", name.getTimeJoined().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
+                .setThumbnail(user.getAvatarUrl())
+                .setAuthor("Profile of " + user.getName(), null, user.getAvatarUrl())
+                .addField("Username:", user.getName(), false)
+                .addField("Account Created:", user.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
+                .addField("Server Joined:", e.getGuild().getMember(user).getTimeJoined().format(DateTimeFormatter.RFC_1123_DATE_TIME), false)
                 .addField("Adores " + emote.getAsMention() + ":", adores, false)
                 .addField("Credits 💰:", credits, false);
 
--- src/main/java/me/agramon/albedo/commands/nsfw/Boobs.java
+++ src/main/java/me/agramon/albedo/commands/nsfw/Boobs.java
@@ -1,20 +1,44 @@
 package me.agramon.albedo.commands.nsfw;
 
+import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import me.agramon.albedo.api.NekosLifeAPI;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import me.duncte123.botcommons.web.WebUtils;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
-public class Boobs extends NekosLifeAPI {
+import java.awt.*;
+
+public class Boobs extends Command {
     public Boobs() {
-        super("boobs", "O.O", 5, "boobs");
-        super.aliases = new String[]{"oppai"};
+        super.name = "boobs";
+        super.help = "O.O";
+        super.cooldown = 5;
+        super.category = new Category("NSFW");
+        super.aliases = new String[]{"tits", "oppai"};
     }
 
     @Override
     protected void execute(CommandEvent e) {
-        if (e.getMessage().getTextChannel().isNSFW()) {
-            super.execute(e);
-        } else {
+        if (!e.getMessage().getTextChannel().isNSFW()) {
             e.reply("This is not a NSFW channel!");
+            return;
         }
+
+        String url;
+        if (e.getArgs().equalsIgnoreCase("gif")) {
+            url = "https://nekos.life/api/v2/img/boobs";
+        } else {
+            url = "https://nekos.life/api/v2/img/tits";
+        }
+
+        WebUtils.ins.getJSONObject(url).async((json) -> {
+            String image = json.get("url").asText();
+            MessageEmbed embed;
+            embed = EmbedUtils.embedImage(image)
+                    .setColor(Color.MAGENTA)
+                    .build();
+            e.reply(embed);
+        });
     }
 }
